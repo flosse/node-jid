aeef575f9775e455acd764c23b4956157df1033c
Markus Kohlhase <mail@markus-kohlhase.de>
2015-01-09 19:56:17 +0000

    use coffee-script's class keyword

--- jid.coffee
+++ jid.coffee
@@ -9,165 +9,167 @@
 # @see http://xmpp.org/extensions/xep-0106.html
 ###
 
-JID = (a, b, c) ->
-  @local = @domain = @resource = null
+class JID
 
-  if a and (not b) and (not c)
-    @parseJID a
-  else if b
-    @setLocal a
-    @setDomain b
-    @setResource c
-  else
-    throw new Error 'Argument error'
+  constructor: (a, b, c) ->
+    @local = @domain = @resource = null
 
-JID::parseJID = (s) ->
+    if a and (not b) and (not c)
+      @parseJID a
+    else if b
+      @setLocal a
+      @setDomain b
+      @setResource c
+    else
+      throw new Error 'Argument error'
 
-  if s.indexOf('@') >= 0
-    @setLocal s.substr 0, s.indexOf '@'
-    s = s.substr s.indexOf('@') + 1
+  parseJID: (s) ->
 
-  if s.indexOf('/') >= 0
-    @setResource s.substr s.indexOf('/') + 1
-    s = s.substr 0, s.indexOf '/'
-  @setDomain s
+    if s.indexOf('@') >= 0
+      @setLocal s.substr 0, s.indexOf '@'
+      s = s.substr s.indexOf('@') + 1
 
-JID::toString = (unescape) ->
-  s = @domain
-  if @local    then s = @getLocal(unescape) + '@' + s
-  if @resource then s = s + '/' + @resource
-  s
+    if s.indexOf('/') >= 0
+      @setResource s.substr s.indexOf('/') + 1
+      s = s.substr 0, s.indexOf '/'
+    @setDomain s
 
-###*
-# Convenience method to distinguish users
-###
-JID::bare = ->
-  if @resource
-    new JID @local, @domain, null
-  else
+  toString: (unescape) ->
+    s = @domain
+    if @local    then s = @getLocal(unescape) + '@' + s
+    if @resource then s = s + '/' + @resource
+    s
+
+  ###*
+  # Convenience method to distinguish users
+  ###
+  bare: ->
+    if @resource
+      new JID @local, @domain, null
+    else
+      @
+
+  ###*
+  # Comparison function
+  ###
+  equals: (other) ->
+    (@local     is other.local    ) and
+    (@domain    is other.domain   ) and
+    (@resource  is other.resource )
+
+  ### Deprecated, use setLocal() [see RFC6122] ###
+  setUser: (user) -> @setLocal user
+
+  ###*
+  # Setters that do stringprep normalization.
+  ###
+  setLocal: (local, escape) ->
+    escape = escape or @detectEscape local
+
+    if escape
+      local = @escapeLocal local
+
+    @local = @user = local and @prep 'nodeprep', local
     @
 
-###*
-# Comparison function
-###
-JID::equals = (other) ->
-  (@local     is other.local    ) and
-  (@domain    is other.domain   ) and
-  (@resource  is other.resource )
+  ###*
+  # http://xmpp.org/rfcs/rfc6122.html#addressing-domain
+  ###
+  setDomain: (domain) ->
+    @domain =
+      domain and @prep 'nameprep', domain.split('.').map(toUnicode).join '.'
+    @
 
-### Deprecated, use setLocal() [see RFC6122] ###
-JID::setUser = (user) -> @setLocal user
+  setResource: (resource) ->
+    @resource = resource and @prep('resourceprep', resource)
+    @
 
-###*
-# Setters that do stringprep normalization.
-###
-JID::setLocal = (local, escape) ->
-  escape = escape or @detectEscape local
+  getLocal: (unescape) ->
+    unescape = unescape or false
 
-  if escape
-    local = @escapeLocal local
+    if unescape
+      @unescapeLocal(@local)
+    else
+      @local
 
-  @local = @user = local and @prep 'nodeprep', local
-  @
+  prep: (operation, value) -> (new StringPrep operation).prepare value
 
-###*
-# http://xmpp.org/rfcs/rfc6122.html#addressing-domain
-###
-JID::setDomain = (domain) ->
-  @domain =
-    domain and @prep 'nameprep', domain.split('.').map(toUnicode).join '.'
-  @
+  ### Deprecated, use getLocal() [see RFC6122] ###
+  getUser: -> @getLocal()
 
-JID::setResource = (resource) ->
-  @resource = resource and @prep('resourceprep', resource)
-  @
+  getDomain: -> @domain
 
-JID::getLocal = (unescape) ->
-  unescape = unescape or false
+  getResource: -> @resource
 
-  if unescape
-    @unescapeLocal(@local)
-  else
-    @local
+  detectEscape: (local) ->
 
-JID::prep = (operation, value) -> (new StringPrep operation).prepare value
+    return false unless local
 
-### Deprecated, use getLocal() [see RFC6122] ###
-JID::getUser = -> @getLocal()
+    # remove all escaped secquences
+    tmp = local.replace /\\20/g, ''
+      .replace /\\22/g, ''
+      .replace /\\26/g, ''
+      .replace /\\27/g, ''
+      .replace /\\2f/g, ''
+      .replace /\\3a/g, ''
+      .replace /\\3c/g, ''
+      .replace /\\3e/g, ''
+      .replace /\\40/g, ''
+      .replace /\\5c/g, ''
 
-JID::getDomain = -> @domain
+    ### detect if we have unescaped sequences ###
+    search = tmp.search /\\| |\"|\&|\'|\/|:|<|>|@/g
+    if search is -1
+      false
+    else
+      true
 
-JID::getResource = -> @resource
+  ###*
+  # Escape the local part of a JID.
+  #
+  # @see http://xmpp.org/extensions/xep-0106.html
+  # @param String local local part of a jid
+  # @return An escaped local part
+  ###
+  escapeLocal: (local) ->
 
-JID::detectEscape = (local) ->
+    return null if local is null
 
-  return false unless local
+    ### jshint -W044 ###
+    local.replace /^\s+|\s+$/g, ''
+      .replace /\\/g, '\\5c'
+      .replace /\s/g, '\\20'
+      .replace /\"/g, '\\22'
+      .replace /\&/g, '\\26'
+      .replace /\'/g, '\\27'
+      .replace /\//g, '\\2f'
+      .replace /:/g,  '\\3a'
+      .replace /</g,  '\\3c'
+      .replace />/g,  '\\3e'
+      .replace /@/g,  '\\40'
+      .replace /\3a/g,'\\5c3a'
 
-  # remove all escaped secquences
-  tmp = local.replace /\\20/g, ''
-    .replace /\\22/g, ''
-    .replace /\\26/g, ''
-    .replace /\\27/g, ''
-    .replace /\\2f/g, ''
-    .replace /\\3a/g, ''
-    .replace /\\3c/g, ''
-    .replace /\\3e/g, ''
-    .replace /\\40/g, ''
-    .replace /\\5c/g, ''
+  ###*
+  # Unescape a local part of a JID.
+  #
+  # @see http://xmpp.org/extensions/xep-0106.html
+  # @param String local local part of a jid
+  # @return unescaped local part
+  ###
+  unescapeLocal: (local) ->
 
-  ### detect if we have unescaped sequences ###
-  search = tmp.search /\\| |\"|\&|\'|\/|:|<|>|@/g
-  if search is -1
-    false
-  else
-    true
+    return null if local is null
 
-###*
-# Escape the local part of a JID.
-#
-# @see http://xmpp.org/extensions/xep-0106.html
-# @param String local local part of a jid
-# @return An escaped local part
-###
-JID::escapeLocal = (local) ->
-
-  return null if local is null
-
-  ### jshint -W044 ###
-  local.replace /^\s+|\s+$/g, ''
-    .replace /\\/g, '\\5c'
-    .replace /\s/g, '\\20'
-    .replace /\"/g, '\\22'
-    .replace /\&/g, '\\26'
-    .replace /\'/g, '\\27'
-    .replace /\//g, '\\2f'
-    .replace /:/g,  '\\3a'
-    .replace /</g,  '\\3c'
-    .replace />/g,  '\\3e'
-    .replace /@/g,  '\\40'
-    .replace /\3a/g,'\\5c3a'
-
-###*
-# Unescape a local part of a JID.
-#
-# @see http://xmpp.org/extensions/xep-0106.html
-# @param String local local part of a jid
-# @return unescaped local part
-###
-JID::unescapeLocal = (local) ->
-
-  return null if local is null
-
-  local.replace /\\20/g, ' '
-    .replace /\\22/g, '\"'
-    .replace /\\26/g, '&'
-    .replace /\\27/g, '\''
-    .replace /\\2f/g, '/'
-    .replace /\\3a/g, ':'
-    .replace /\\3c/g, '<'
-    .replace /\\3e/g, '>'
-    .replace /\\40/g, '@'
-    .replace /\\5c/g, '\\'
+    local.replace /\\20/g, ' '
+      .replace /\\22/g, '\"'
+      .replace /\\26/g, '&'
+      .replace /\\27/g, '\''
+      .replace /\\2f/g, '/'
+      .replace /\\3a/g, ':'
+      .replace /\\3c/g, '<'
+      .replace /\\3e/g, '>'
+      .replace /\\40/g, '@'
+      .replace /\\5c/g, '\\'
 
 if exports?     then  module.exports = JID
 else if window? then  window.JID = JID
--- jid.js
+++ jid.js
@@ -14,178 +14,183 @@
    * @see http://xmpp.org/extensions/xep-0106.html
    */
 
-  JID = function(a, b, c) {
-    this.local = this.domain = this.resource = null;
-    if (a && (!b) && (!c)) {
-      return this.parseJID(a);
-    } else if (b) {
-      this.setLocal(a);
-      this.setDomain(b);
-      return this.setResource(c);
-    } else {
-      throw new Error('Argument error');
+  JID = (function() {
+    function JID(a, b, c) {
+      this.local = this.domain = this.resource = null;
+      if (a && (!b) && (!c)) {
+        this.parseJID(a);
+      } else if (b) {
+        this.setLocal(a);
+        this.setDomain(b);
+        this.setResource(c);
+      } else {
+        throw new Error('Argument error');
+      }
     }
-  };
 
-  JID.prototype.parseJID = function(s) {
-    if (s.indexOf('@') >= 0) {
-      this.setLocal(s.substr(0, s.indexOf('@')));
-      s = s.substr(s.indexOf('@') + 1);
-    }
-    if (s.indexOf('/') >= 0) {
-      this.setResource(s.substr(s.indexOf('/') + 1));
-      s = s.substr(0, s.indexOf('/'));
-    }
-    return this.setDomain(s);
-  };
+    JID.prototype.parseJID = function(s) {
+      if (s.indexOf('@') >= 0) {
+        this.setLocal(s.substr(0, s.indexOf('@')));
+        s = s.substr(s.indexOf('@') + 1);
+      }
+      if (s.indexOf('/') >= 0) {
+        this.setResource(s.substr(s.indexOf('/') + 1));
+        s = s.substr(0, s.indexOf('/'));
+      }
+      return this.setDomain(s);
+    };
 
-  JID.prototype.toString = function(unescape) {
-    var s;
-    s = this.domain;
-    if (this.local) {
-      s = this.getLocal(unescape) + '@' + s;
-    }
-    if (this.resource) {
-      s = s + '/' + this.resource;
-    }
-    return s;
-  };
+    JID.prototype.toString = function(unescape) {
+      var s;
+      s = this.domain;
+      if (this.local) {
+        s = this.getLocal(unescape) + '@' + s;
+      }
+      if (this.resource) {
+        s = s + '/' + this.resource;
+      }
+      return s;
+    };
 
 
-  /**
-   * Convenience method to distinguish users
-   */
+    /**
+     * Convenience method to distinguish users
+     */
 
-  JID.prototype.bare = function() {
-    if (this.resource) {
-      return new JID(this.local, this.domain, null);
-    } else {
+    JID.prototype.bare = function() {
+      if (this.resource) {
+        return new JID(this.local, this.domain, null);
+      } else {
+        return this;
+      }
+    };
+
+
+    /**
+     * Comparison function
+     */
+
+    JID.prototype.equals = function(other) {
+      return (this.local === other.local) && (this.domain === other.domain) && (this.resource === other.resource);
+    };
+
+
+    /* Deprecated, use setLocal() [see RFC6122] */
+
+    JID.prototype.setUser = function(user) {
+      return this.setLocal(user);
+    };
+
+
+    /**
+     * Setters that do stringprep normalization.
+     */
+
+    JID.prototype.setLocal = function(local, escape) {
+      escape = escape || this.detectEscape(local);
+      if (escape) {
+        local = this.escapeLocal(local);
+      }
+      this.local = this.user = local && this.prep('nodeprep', local);
       return this;
-    }
-  };
+    };
 
 
-  /**
-   * Comparison function
-   */
+    /**
+     * http://xmpp.org/rfcs/rfc6122.html#addressing-domain
+     */
 
-  JID.prototype.equals = function(other) {
-    return (this.local === other.local) && (this.domain === other.domain) && (this.resource === other.resource);
-  };
+    JID.prototype.setDomain = function(domain) {
+      this.domain = domain && this.prep('nameprep', domain.split('.').map(toUnicode).join('.'));
+      return this;
+    };
 
+    JID.prototype.setResource = function(resource) {
+      this.resource = resource && this.prep('resourceprep', resource);
+      return this;
+    };
 
-  /* Deprecated, use setLocal() [see RFC6122] */
+    JID.prototype.getLocal = function(unescape) {
+      unescape = unescape || false;
+      if (unescape) {
+        return this.unescapeLocal(this.local);
+      } else {
+        return this.local;
+      }
+    };
 
-  JID.prototype.setUser = function(user) {
-    return this.setLocal(user);
-  };
-
-
-  /**
-   * Setters that do stringprep normalization.
-   */
-
-  JID.prototype.setLocal = function(local, escape) {
-    escape = escape || this.detectEscape(local);
-    if (escape) {
-      local = this.escapeLocal(local);
-    }
-    this.local = this.user = local && this.prep('nodeprep', local);
-    return this;
-  };
+    JID.prototype.prep = function(operation, value) {
+      return (new StringPrep(operation)).prepare(value);
+    };
 
 
-  /**
-   * http://xmpp.org/rfcs/rfc6122.html#addressing-domain
-   */
+    /* Deprecated, use getLocal() [see RFC6122] */
 
-  JID.prototype.setDomain = function(domain) {
-    this.domain = domain && this.prep('nameprep', domain.split('.').map(toUnicode).join('.'));
-    return this;
-  };
+    JID.prototype.getUser = function() {
+      return this.getLocal();
+    };
 
-  JID.prototype.setResource = function(resource) {
-    this.resource = resource && this.prep('resourceprep', resource);
-    return this;
-  };
+    JID.prototype.getDomain = function() {
+      return this.domain;
+    };
 
-  JID.prototype.getLocal = function(unescape) {
-    unescape = unescape || false;
-    if (unescape) {
-      return this.unescapeLocal(this.local);
-    } else {
-      return this.local;
-    }
-  };
+    JID.prototype.getResource = function() {
+      return this.resource;
+    };
 
-  JID.prototype.prep = function(operation, value) {
-    return (new StringPrep(operation)).prepare(value);
-  };
+    JID.prototype.detectEscape = function(local) {
+      var search, tmp;
+      if (!local) {
+        return false;
+      }
+      tmp = local.replace(/\\20/g, '').replace(/\\22/g, '').replace(/\\26/g, '').replace(/\\27/g, '').replace(/\\2f/g, '').replace(/\\3a/g, '').replace(/\\3c/g, '').replace(/\\3e/g, '').replace(/\\40/g, '').replace(/\\5c/g, '');
 
-
-  /* Deprecated, use getLocal() [see RFC6122] */
-
-  JID.prototype.getUser = function() {
-    return this.getLocal();
-  };
-
-  JID.prototype.getDomain = function() {
-    return this.domain;
-  };
-
-  JID.prototype.getResource = function() {
-    return this.resource;
-  };
-
-  JID.prototype.detectEscape = function(local) {
-    var search, tmp;
-    if (!local) {
-      return false;
-    }
-    tmp = local.replace(/\\20/g, '').replace(/\\22/g, '').replace(/\\26/g, '').replace(/\\27/g, '').replace(/\\2f/g, '').replace(/\\3a/g, '').replace(/\\3c/g, '').replace(/\\3e/g, '').replace(/\\40/g, '').replace(/\\5c/g, '');
-
-    /* detect if we have unescaped sequences */
-    search = tmp.search(/\\| |\"|\&|\'|\/|:|<|>|@/g);
-    if (search === -1) {
-      return false;
-    } else {
-      return true;
-    }
-  };
+      /* detect if we have unescaped sequences */
+      search = tmp.search(/\\| |\"|\&|\'|\/|:|<|>|@/g);
+      if (search === -1) {
+        return false;
+      } else {
+        return true;
+      }
+    };
 
 
-  /**
-   * Escape the local part of a JID.
-   *
-   * @see http://xmpp.org/extensions/xep-0106.html
-   * @param String local local part of a jid
-   * @return An escaped local part
-   */
+    /**
+     * Escape the local part of a JID.
+     *
+     * @see http://xmpp.org/extensions/xep-0106.html
+     * @param String local local part of a jid
+     * @return An escaped local part
+     */
 
-  JID.prototype.escapeLocal = function(local) {
-    if (local === null) {
-      return null;
-    }
+    JID.prototype.escapeLocal = function(local) {
+      if (local === null) {
+        return null;
+      }
 
-    /* jshint -W044 */
-    return local.replace(/^\s+|\s+$/g, '').replace(/\\/g, '\\5c').replace(/\s/g, '\\20').replace(/\"/g, '\\22').replace(/\&/g, '\\26').replace(/\'/g, '\\27').replace(/\//g, '\\2f').replace(/:/g, '\\3a').replace(/</g, '\\3c').replace(/>/g, '\\3e').replace(/@/g, '\\40').replace(/\3a/g, '\\5c3a');
-  };
+      /* jshint -W044 */
+      return local.replace(/^\s+|\s+$/g, '').replace(/\\/g, '\\5c').replace(/\s/g, '\\20').replace(/\"/g, '\\22').replace(/\&/g, '\\26').replace(/\'/g, '\\27').replace(/\//g, '\\2f').replace(/:/g, '\\3a').replace(/</g, '\\3c').replace(/>/g, '\\3e').replace(/@/g, '\\40').replace(/\3a/g, '\\5c3a');
+    };
 
 
-  /**
-   * Unescape a local part of a JID.
-   *
-   * @see http://xmpp.org/extensions/xep-0106.html
-   * @param String local local part of a jid
-   * @return unescaped local part
-   */
+    /**
+     * Unescape a local part of a JID.
+     *
+     * @see http://xmpp.org/extensions/xep-0106.html
+     * @param String local local part of a jid
+     * @return unescaped local part
+     */
 
-  JID.prototype.unescapeLocal = function(local) {
-    if (local === null) {
-      return null;
-    }
-    return local.replace(/\\20/g, ' ').replace(/\\22/g, '\"').replace(/\\26/g, '&').replace(/\\27/g, '\'').replace(/\\2f/g, '/').replace(/\\3a/g, ':').replace(/\\3c/g, '<').replace(/\\3e/g, '>').replace(/\\40/g, '@').replace(/\\5c/g, '\\');
-  };
+    JID.prototype.unescapeLocal = function(local) {
+      if (local === null) {
+        return null;
+      }
+      return local.replace(/\\20/g, ' ').replace(/\\22/g, '\"').replace(/\\26/g, '&').replace(/\\27/g, '\'').replace(/\\2f/g, '/').replace(/\\3a/g, ':').replace(/\\3c/g, '<').replace(/\\3e/g, '>').replace(/\\40/g, '@').replace(/\\5c/g, '\\');
+    };
+
+    return JID;
+
+  })();
 
   if (typeof exports !== "undefined" && exports !== null) {
     module.exports = JID;
